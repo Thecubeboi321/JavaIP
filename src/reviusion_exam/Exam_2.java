2002ebc3c3247bd9cc3458986bb6cc33d06c6a3e
Thecubeboi321 <uargei123@gmail.com>
2026-06-04 10:21:13 +0900

    Testing commit 2

--- src/reviusion_exam/Exam_2.java
+++ src/reviusion_exam/Exam_2.java
@@ -6,8 +6,8 @@ public class Exam_2 {
     public static void main(String[] args) {
         //Vars
         final double REQ_TRAB = 0.1; // THis eclare a double anbd to test file changes
-        final double REQ_EM = 0.4; 
-        final double REQ_EF = 0.5; 
+        final double REQ_EM = 0.4;  // This is Exame medio 
+        final double REQ_EF = 0.5;  // This is exame Final 
                 
         double valorTrab, valorEM, valorEF, somTot, somaTotal;
         String naran_est, disc;
